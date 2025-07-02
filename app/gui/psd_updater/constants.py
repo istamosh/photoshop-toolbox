36edc35b67c50f0f22b15ab10f26c5582b7b6271
istamosh <63596969+istamosh@users.noreply.github.com>
2025-07-02 17:49:38 +0700

    adjusted the date updater size to be smaller, will add sliders

--- app/gui/psd_updater/constants.py
+++ app/gui/psd_updater/constants.py
@@ -19,8 +19,8 @@ class Justification(Enum):
 class TextSizing:
     """Text sizing constants."""
 
-    TEXT_SIZE_RATIO = 0.035  # 3.5% of smaller dimension
-    LEADING_RATIO = 1.2  # 120% of text size
+    TEXT_SIZE_RATIO = 0.02583  # 2.58% of smaller dimension (31pt for 1200px)
+    LEADING_RATIO = 1.129  # 112.9% of text size (35pt for 31pt font)
     PADDING_RATIO = 0.03  # 3% of smaller dimension
 
 
